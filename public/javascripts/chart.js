90af25797de7f981a8ea16e976a262d303559c18
naoty <naoty.k@gmail.com>
2012-12-24 01:12:43 +0900

    Update chart through websocket

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -1,14 +1,39 @@
 $(function () {
+    // localize timezone
+    Highcharts.setOptions({
+        global: {
+            useUTC: false
+        }
+    });
+
     var chart = new Highcharts.Chart({
         chart: {
             renderTo: 'chart',
-            type: 'line'
+            type: 'spline'
+        },
+        title: {
+            text: 'curl -X POST http://makura.herouapp.com'
         },
         xAxis: {
             type: 'datetime'
         },
+        yAxis: {
+            min: 0,
+            max: 1
+        },
         series: [{
-            data: [1, 3, 5, 10]
+            data: [
+                getRandomPoint()
+            ]
         }]
     });
+
+    var socket = io.connect();
+    socket.on('stream', function (data) {
+        chart.series[0].addPoint(getRandomPoint(), true, false);
+    });
+
+    function getRandomPoint() {
+        return [(new Date()).getTime(), Math.random()];
+    }
 });
